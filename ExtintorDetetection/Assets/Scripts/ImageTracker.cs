89465880450ec1b2ef73166288848c9a5053cb42
Joao Guimaraes <jssgmrs22@gmail.com>
2020-01-15 15:40:04 +0000

    Add image tracking

---
+++ ExtintorDetetection/Assets/Scripts/ImageTracker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageTracker : MonoBehaviour
+{
+    public GameObject Form;
+    private ARTrackedImageManager _artracker;
+
+    private void Awake()
+    {
+        _artracker = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        _artracker.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        _artracker.trackedImagesChanged -= OnImageChanged;
+    }
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var img in args.added)
+        {
+            Form.SetActive(true);
+            Debug.Log(img.name);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ExtintorDetetection/Assets/Scripts/NotAppear.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class NotAppear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject myForm;
+    private ARTrackedImageManager _manager;
+
+    private void Awake()
+    {
+        _manager = FindObjectOfType<ARTrackedImageManager>();
+        myForm.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        _manager.trackedImagesChanged += OnImgChanged;
+    }
+
+    private void OnDisable()
+    {
+        _manager.trackedImagesChanged -= OnImgChanged;
+    }
+
+    private void OnImgChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var img in args.added)
+        {
+            myForm.SetActive(true);
+            Debug.Log(img.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
