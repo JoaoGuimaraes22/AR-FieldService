dd2f49808493f9287cfb4445fcada4b82cc85b28
Joao Guimaraes <jssgmrs22@gmail.com>
2020-01-15 16:35:57 +0000

    (Trying) to save form data

--- ExtintorDetetection/Assets/Scripts/GetData.cs
+++ ExtintorDetetection/Assets/Scripts/GetData.cs
@@ -1,12 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GetData : MonoBehaviour
 {
+
+    public InputField _input;
+    public Toggle Validade;
+    public Toggle Danos;
+    public Toggle Localizacao;
+    public Toggle Instrucoes;
+    public Text debug;
+
+    public void HandleClick()
+    {
+        string comment = _input.text;
+        bool validade = Validade.isOn;
+        bool danos = Danos.isOn;
+        bool localizacao = Localizacao.isOn;
+        bool instrucoes = Instrucoes.isOn;
+
+        debug.text = $"This is the text now: {comment} {validade.ToString()} {danos.ToString()} {localizacao.ToString()} {instrucoes.ToString()}";
+
+
+
+
+    } 
+
     // Start is called before the first frame update
     void Start()
     {
+        _input = FindObjectOfType<InputField>();
         
     }
 
