c30d737a6f70513f6c630a0baeeb8fe3bfe1a14c
Joao Guimaraes <jssgmrs22@gmail.com>
2020-01-15 17:10:54 +0000

    Finally managed to fetch data from forms in UI form app

---
+++ ExtintorDetetection/Assets/Scripts/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+
+    public Text txt;
+    public InputField input;
+    public Toggle val;
+    public Toggle dan;
+    public Toggle loc;
+    public Toggle instr;
+
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        txt.text = "This a text";
+    }
+
+    public void ChangeText()
+    {
+        txt.text = $"Working: {input.text}, {val.isOn.ToString()}, {dan.isOn.ToString()}, {loc.isOn.ToString()}, {instr.isOn.ToString()},";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
